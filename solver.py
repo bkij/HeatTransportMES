5377a47894fa63c4d55a68de3fab5158e889fee8
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-14 12:39:45 +0100

    Add starting code for the program

---
+++ solver.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# TODO: python2 or python3?
+
+from sys import argv, exit
+import numpy as np
+import matplotlib.pyplot as plt
+
+def print_usage():
+    print("Usage: ./solver [num_elements] ")
+    print()
+    print("Solves a heat transport DE using the MES method")
+    print("The num_elements parameter constitutes the number of finite elements")
+    print("used for approximation")
+    print()
+
+def parse_arguments():
+    if len(argv) < 2:
+        print_usage()
+        exit()
+    num_elements = int(argv[1])
+    return num_elements
+
+def main():
+    num_elements = parse_arguments()
+    pass # TODO: Implement
+
+if __name__ == '__main__':
+    main()
